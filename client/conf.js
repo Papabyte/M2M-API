3e0de8212dd52f4047cab526efb5dd4adcbbf1af
Papabyte <bcreze@protonmail.com>
2019-09-03 12:21:28 +0200

    Initial commit

---
+++ client/conf.js
@@ -0,0 +1,28 @@
+exports.bServeAsHub = false;
+exports.bLight = true;
+exports.bSingleAddress = true;
+
+exports.WS_PROTOCOL = "wss://";
+exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
+exports.deviceName = 'paid-access-API';
+exports.permanent_pairing_secret = '0000';
+exports.control_addresses = [''];
+
+exports.isHighAvaibilityNode =  false;
+exports.minChannelTimeoutInSecond = 600;
+exports.maxChannelTimeoutInSecond = 1000;
+exports.defaultTimeoutInSecond = 600;
+
+exports.unconfirmedAmountsLimitsByAssetOrChannel = {
+	"base" : {
+		max_unconfirmed_by_asset : 1e6,
+		max_unconfirmed_by_channel : 1e6,
+		minimum_time_in_second : 5
+	}
+}
+
+exports.isHighAvaibilityNode =  false;
+
+exports.enabledReceivers = ['obyte-messenger'];
+
+console.log('finished API client conf');
---
+++ server/conf.js
@@ -0,0 +1,27 @@
+exports.bServeAsHub = false;
+exports.bLight = true;
+exports.bSingleAddress = true;
+
+exports.WS_PROTOCOL = "wss://";
+exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
+exports.deviceName = 'API server';
+exports.permanent_pairing_secret = '0000';
+exports.control_addresses = [''];
+
+exports.minChannelTimeoutInSecond = 600;
+exports.maxChannelTimeoutInSecond = 1000;
+exports.defaultTimeoutInSecond = 600;
+
+exports.unconfirmedAmountsLimitsByAssetOrChannel = {
+	"base" : {
+		max_unconfirmed_by_asset : 1e6,
+		max_unconfirmed_by_channel : 1e6,
+		minimum_time_in_second : 5
+	}
+}
+
+exports.isHighAvaibilityNode =  false;
+
+exports.enabledReceivers = ['obyte-messenger'];
+
+console.log('finished API server conf');
