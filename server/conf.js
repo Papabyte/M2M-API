d3a2a849a479a386fbba8b9fc4a1d9481811e8f0
papabyte <bcreze@protonmail.com>
2020-08-03 04:25:19 +0200

    update confs

--- server/conf.js
+++ server/conf.js
@@ -8,15 +8,15 @@ exports.deviceName = 'API server';
 exports.permanent_pairing_secret = '0000';
 exports.control_addresses = [''];
 
-exports.minChannelTimeoutInSecond = 600;
-exports.maxChannelTimeoutInSecond = 1000;
-exports.defaultTimeoutInSecond = 900;
+exports.minChannelTimeoutInSeconds = 600;
+exports.maxChannelTimeoutInSeconds = 1000;
+exports.defaultTimeoutInSeconds = 900;
 
 exports.unconfirmedAmountsLimitsByAssetOrChannel = {
 	"base" : {
 		max_unconfirmed_by_asset : 1e6,
 		max_unconfirmed_by_channel : 1e6,
-		minimum_time_in_second : 5
+		minimum_time_in_seconds : 5
 	}
 }
 
